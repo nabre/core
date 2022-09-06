a718f8fc432c6b51ab8ad82d84983802dcbc382d
nabre <nabrejunior@gmail.com>
2022-09-06 23:42:25 +0200

    update

---
+++ files/app/Models/User.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Models;
+
+use Nabre\Models\User as ModelsUser;
+
+class User extends ModelsUser
+{
+}
---
+++ src/Console/Commands/InstallPkgFiles.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Nabre\Console\Commands;
+
+use Illuminate\Console\Command;
+use Nabre\Database\MongoDB\Backup\Execute;
+use Storage;
+
+class InstallPkgFiles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'nabrecore:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Installazione dei contenuti principali del pacchetto nabre/core';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {        
+        $this->info("Installazione");
+
+        $directory='files';
+        $files=Storage::allFiles($directory);
+        dd($files);
+    }
+}
