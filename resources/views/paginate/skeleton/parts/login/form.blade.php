62965c80bc4a5ef851ac66d64cdab84c36ede9a6
nabre <nabrejunior@gmail.com>
2022-09-11 07:40:34 +0200

    update

--- resources/views/paginate/skeleton/parts/login/form.blade.php
+++ resources/views/paginate/skeleton/parts/login/form.blade.php
@@ -3,16 +3,16 @@
 
     <!-- Email Address -->
           <div class="mb-3 row">
-            <label for="inputMail" class="col-sm-2 col-form-label">{{__('Email')}}</label>
-            <div class="col-sm-10">
+            <label for="inputMail" class="col-md-3 col-form-label">{{__("Email Address")}}</label>
+            <div class="col">
               <input class="form-control" required id="inputMail" value="{{old('email')}}" type="email" name="email">
             </div>
           </div>
 
     <!-- Password -->
           <div class="mb-3 row">
-            <label for="inputPsw" class="col-sm-2 col-form-label">{{__('Password')}}</label>
-            <div class="col-sm-10">
+            <label for="inputPsw" class="col-md-3 col-form-label">{{__('Password')}}</label>
+            <div class="col">
               <input class="form-control" autocomplete="current-password" required id="inputPsw" type="password" name="password">
             </div>
           </div>
@@ -21,13 +21,13 @@
     <div class="form-check">
       <input class="form-check-input" type="checkbox" value="" id="remember_me" name="remember">
       <label class="form-check-label" for="remember_me">
-        {{ __('loginpage.remember') }}
+        {{ __("Remember Me") }}
       </label>
     </div>
 
     @if (Route::has('password.request'))
         <a href="{{ route('password.request') }}">
-            {{ __('loginpage.forgot') }}
+            {{ __("Forgot Your Password?") }}
         </a>
     @endif
 
@@ -35,7 +35,7 @@
     <div class="col-sm-2 col-form-label"></div>
     <div class="col-sm-10">
         <button class="btn btn-primary" type="submit">
-             {{ __('loginpage.button') }}
+             {{ __("Login") }}
         </button>
     </div>
   </div>
