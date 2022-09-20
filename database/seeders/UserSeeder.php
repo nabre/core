93605556b287ab50d6d264c3aa6d9513acf94b10
nabre <nabrejunior@gmail.com>
2022-09-20 11:17:06 +0200

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,7 +21,7 @@ class UserSeeder extends Seeder
             $node = User::create();
             $data = [
                 'name' => 'Admin account',
-                'email' => 'admin@admin.test',
+                'email' => 'admin@account.test',
                 'password' => 'password',
                 "email_verified_at" => Carbon::now(),
                 'roles' => [
