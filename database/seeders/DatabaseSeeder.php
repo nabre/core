1d5a50307071806dd86194d262183d164b313916
nabre <nabrejunior@gmail.com>
2022-09-21 16:41:55 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,19 +3,21 @@
 namespace Nabre\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
     function run()
     {
+        Artisan::call('optimize');
         $this->call([
             RoleSeeder::class,
+            UserSeeder::class,
             FormFieldsTypeSeeder::class,
             SettingSeeder::class,
             SettingGroupSeeder::class,
             PageSeeder::class,
             MenuSeeder::class,
-            UserSeeder::class,
         ]);
     }
 }
