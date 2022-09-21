810fb1a86c212f4760d73c788a636f96c14532ea
nabre <nabrejunior@gmail.com>
2022-09-21 17:07:37 +0200

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -27,6 +27,7 @@ class UserSeeder extends Seeder
             ]);
 
             if (!is_null(data_get($data, 'email')) && !is_null(data_get($data, 'password'))) {
+                dd($data);
                 $node = User::create();
                 $node->recursiveSave($data);
             }
