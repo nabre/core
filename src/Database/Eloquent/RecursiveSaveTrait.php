fdcd484b2eb5bc503b85bc0d9964a45ab08e604f
nabre <nabrejunior@gmail.com>
2022-09-23 00:06:25 +0200

    Update RecursiveSaveTrait.php

--- src/Database/Eloquent/RecursiveSaveTrait.php
+++ src/Database/Eloquent/RecursiveSaveTrait.php
@@ -59,7 +59,8 @@ trait RecursiveSaveTrait
     {
         $model = $this;
 
-        $model->fill(['name'=>'ciao'], ['upsert' => true]);
+        dd(get_defined_vars());
+        $model->fill($data, ['upsert' => true]);
         $model->save();
 /*
         #carica model contronto getKeyName()
