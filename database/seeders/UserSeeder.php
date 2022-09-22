aaabb3c633a5d8e57b3ef4f571b08a2613abcb6a
nabre <nabrejunior@gmail.com>
2022-09-22 05:36:06 +0200

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -30,9 +30,9 @@ class UserSeeder extends Seeder
                 'email'=>'admin@account.test',
                 'password'=>'password',
                 "email_verified_at"=> Carbon::now(),
-                'roles'=> [
+               /* 'roles'=> [
                     data_get(Role::where('priority', $minPri)->first(), 'id'),
-                ]
+                ]*/
             ];
 
             if (!is_null(data_get($data, 'email')) && !is_null(data_get($data, 'password'))) {
