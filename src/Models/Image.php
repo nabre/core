28421607e0cc1a372564fa7214cf00a501738fb4
nabre <79878606+nabre@users.noreply.github.com>
2023-01-01 08:47:57 +0100

    update

---
+++ src/Models/Image.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Nabre\Models;
+
+use Nabre\Casts\ImageCodeCast;
+use Nabre\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected $guard_name = 'web';
+    protected $collection = '_Images'; //nome collection
+    protected $fillable = ['code','type']; //campi editabili
+    //  protected $attributes = ['¨slug'=>null,'title'=>null]; //Valori di default
+    protected $casts = ['code'=> ImageCodeCast::class]; //tipo di dato
+    protected $foreignCascade = false;
+
+    function getImageAttribute(){
+        $response = \Response::make($this->code, 200);
+        $response->header("Content-Type", $this->type);
+        return $response;
+    }
+}
+
